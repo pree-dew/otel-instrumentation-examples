9ee733d1ec830d9dfb8a4140f680fbdc72723dc8
Preeti Dewani <preetidewani1990@gmail.com>
2024-11-14 16:41:28 +0530

    sample application for understanding instrumentation

---
+++ cmd/notes/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+
+	"otel-instrumentation/instrumentation"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+)
+
+const url = "http://localhost:8000"
+
+var client *http.Client
+
+func main() {
+	exitCode := 0
+	defer func() {
+		os.Exit(exitCode)
+	}()
+
+	shutdown, err := instrumentation.Run(context.Background(), "notes-cli")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to run OpenTelemetry: %v\n", err)
+	}
+	defer func() {
+		if err := shutdown(context.Background()); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to shutdown OpenTelemetry: %v\n", err)
+		}
+	}()
+
+	// Instrument http.Client with proper options
+	client = &http.Client{
+		Transport: otelhttp.NewTransport(
+			http.DefaultTransport,
+			otelhttp.WithTracerProvider(otel.GetTracerProvider()),
+			otelhttp.WithPropagators(otel.GetTextMapPropagator()),
+		),
+	}
+
+	if len(os.Args) == 1 {
+		printHelp()
+		return
+	}
+
+	switch os.Args[1] {
+	case "list":
+		if len(os.Args) != 2 {
+			fmt.Fprintln(os.Stderr, "Invalid usage")
+			printHelp()
+			exitCode = 1
+			return
+		}
+		err := listTasks()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to list tasks: %v\n", err)
+			exitCode = 1
+			return
+		}
+
+	case "add":
+		if len(os.Args) != 3 {
+			fmt.Fprintln(os.Stderr, "Invalid usage")
+			printHelp()
+			exitCode = 1
+			return
+		}
+		err := addTask(os.Args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to add task: %v\n", err)
+			exitCode = 1
+			return
+		}
+
+	case "update":
+		if len(os.Args) != 4 {
+			fmt.Fprintln(os.Stderr, "Invalid usage")
+			printHelp()
+			exitCode = 1
+			return
+		}
+		n, err := strconv.ParseInt(os.Args[2], 10, 32)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable convert task_num into int32: %v\n", err)
+			exitCode = 1
+			return
+		}
+		err = updateTask(int32(n), os.Args[3])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to update task: %v\n", err)
+			exitCode = 1
+			return
+		}
+
+	case "remove":
+		if len(os.Args) != 3 {
+			fmt.Fprintln(os.Stderr, "Invalid usage")
+			printHelp()
+			exitCode = 1
+			return
+		}
+		n, err := strconv.ParseInt(os.Args[2], 10, 32)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable convert task_num into int32: %v\n", err)
+			exitCode = 1
+			return
+		}
+		err = removeTask(int32(n))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to remove task: %v\n", err)
+			exitCode = 1
+			return
+		}
+
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid command")
+		printHelp()
+		exitCode = 1
+		return
+	}
+}
+
+func listTasks() error {
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return errors.New("HTTP: " + resp.Status)
+	}
+	_, err = io.Copy(os.Stdout, resp.Body)
+	return err
+}
+
+func addTask(description string) error {
+	var buf bytes.Buffer
+	buf.WriteString(description)
+
+	ctx := context.Background()
+	req, err := http.NewRequestWithContext(ctx, "POST", url, &buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		err = errors.New("HTTP: " + resp.Status)
+		return err
+	}
+	return nil
+}
+
+func updateTask(itemNum int32, description string) error {
+	var buf bytes.Buffer
+	buf.WriteString(description)
+	req, _ := http.NewRequest("PUT", url+"/"+strconv.Itoa(int(itemNum)), &buf)
+	req.Header.Set("Content-Type", "text/plain")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return errors.New("HTTP: " + resp.Status)
+	}
+	return nil
+}
+
+func removeTask(itemNum int32) error {
+	req, _ := http.NewRequest("DELETE", url+"/"+strconv.Itoa(int(itemNum)), nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode >= 400 {
+		return errors.New("HTTP: " + resp.Status)
+	}
+	return nil
+}
+
+func printHelp() {
+	fmt.Print(`TODO CLI application
+Usage:
+  todo list
+  todo add task
+  todo update task_num item
+  todo remove task_num
+Example:
+  todo add 'Learn Go'
+  todo list
+`)
+}
---
+++ cmd/notesserver/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+
+	"github.com/XSAM/otelsql"
+	"github.com/gorilla/mux"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+	"go.opentelemetry.io/otel"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+
+	"otel-instrumentation/instrumentation"
+)
+
+var db *sql.DB
+
+func main() {
+	// handle CTRL+C gracefully
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	shutdown, err := instrumentation.Run(ctx, "notes-server")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to run OpenTelemetry: %v\n", err)
+	}
+	defer func() {
+		if err := shutdown(context.Background()); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to shutdown OpenTelemetry: %v\n", err)
+		}
+	}()
+
+	// Instrument database/sql.
+	db, err = otelsql.Open("pgx", "postgres://postgres:pswd@localhost:5434/postgres", otelsql.WithAttributes(semconv.DBSystemPostgreSQL))
+	if err != nil {
+		log.Fatalf("Unable to connection to database: %v\n", err)
+	}
+	defer db.Close()
+
+	err = otelsql.RegisterDBStatsMetrics(db, otelsql.WithAttributes(semconv.DBSystemPostgreSQL))
+	if err != nil {
+		log.Fatalf("Unable to RegisterDBStatsMetrics: %v\n", err)
+	}
+
+	// Add a custom metric.
+	meter := otel.Meter("todoservice")
+	taskCnt, err := meter.Int64UpDownCounter("task.count")
+	if err != nil {
+		log.Fatalf("Unable to create list_tasks metrics counter: %v\n", err)
+	}
+
+	r := mux.NewRouter()
+
+	// Instrument gorilla/mux with OpenTelemetry tracing.
+	r.Use(otelmux.Middleware("mux-server"))
+
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		tasks, err := listTasks(r.Context())
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		for _, t := range tasks {
+			_, _ = fmt.Fprintf(rw, "%d. %s\n", t.id, t.description)
+		}
+	}).Methods("GET")
+
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		desc, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = addTask(r.Context(), string(desc))
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		taskCnt.Add(context.Background(), 1)
+	}).Methods("POST")
+
+	r.HandleFunc("/{id:[0-9]+}", func(rw http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		desc, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = updateTask(r.Context(), int32(id), string(desc))
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}).Methods("PUT")
+
+	r.HandleFunc("/{id:[0-9]+}", func(rw http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = removeTask(r.Context(), int32(id))
+		if err != nil {
+			log.Println(err)
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		taskCnt.Add(context.Background(), -1)
+	}).Methods("DELETE")
+
+	srv := &http.Server{Addr: ":8000", Handler: r}
+
+	go func() {
+		<-ctx.Done()
+		srv.Close()
+	}()
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+type task struct {
+	id          int32
+	description string
+}
+
+func listTasks(ctx context.Context) ([]task, error) {
+	var tasks []task
+	rows, err := db.QueryContext(ctx, "SELECT id, description FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var t task
+		if err := rows.Scan(&t.id, &t.description); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, t)
+	}
+
+	return tasks, rows.Err()
+}
+
+func addTask(ctx context.Context, description string) error {
+	_, err := db.ExecContext(ctx, "INSERT INTO tasks(description) VALUES($1)", description)
+	return err
+}
+
+func updateTask(ctx context.Context, itemNum int32, description string) error {
+	_, err := db.ExecContext(ctx, "UPDATE tasks SET description=$1 WHERE id=$2", description, itemNum)
+	return err
+}
+
+func removeTask(ctx context.Context, itemNum int32) error {
+	_, err := db.ExecContext(ctx, "DELETE FROM tasks WHERE id=$1", itemNum)
+	return err
+}
